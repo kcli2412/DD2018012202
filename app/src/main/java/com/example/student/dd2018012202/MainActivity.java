b2fe18743b0554fa24f9ca014276feb6c410fc2e
kcli2412 <kcli2412@gmail.com>
2018-01-22 14:30:27 +0800

    create SQLite

--- app/src/main/java/com/example/student/dd2018012202/MainActivity.java
+++ app/src/main/java/com/example/student/dd2018012202/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.student.dd2018012202;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyHelper helper = new MyHelper(this);
+        SQLiteDatabase db = helper.getWritableDatabase();
     }
 }
